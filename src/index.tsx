7a326e80a5f043891a4079fc30295007286c469f
Vitor Daniel <vitor036daniel@gmail.com>
2022-11-24 09:35:38 -0300

    add strategies navigation

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,24 @@ import Posts from "./posts"
 import { useInput } from "ink"
 import { Newline } from "ink"
 import { useState } from "react"
+import { Box } from "ink"
 
 const TabNews = () => {
   const [page, setPage] = useState(1)
   const perPage = 10
-  const strategy = "new"
   const [selected, setSelected] = React.useState(0)
 
+  const strategies: ["new", "old", "relevant"] = ["new", "old", "relevant"]
+  const [strategyIndex, setStrategyIndex] = useState(0)
+
   useInput((_input, key) => {
-    if (key.upArrow) {
+    if (key.tab) {
+      if (strategyIndex < strategies.length - 1) {
+        setStrategyIndex(strategyIndex + 1)
+      } else {
+        setStrategyIndex(0)
+      }
+    } else if (key.upArrow) {
       if (selected > 0) {
         setSelected(selected - 1)
       }
@@ -33,9 +42,16 @@ const TabNews = () => {
 
   return (
     <>
-      <Text bold={true}>Página: {page}</Text>
+      <Box display="flex" flexDirection="column">
+        <Text bold={true}>Página: {page}</Text>
+        {strategies.map((strategy, i) => (
+          <Text key={strategy} bold={strategyIndex == i}>
+            {strategy}
+          </Text>
+        ))}
+      </Box>
       <Newline />
-      <Posts page={page} perPage={perPage} strategy={strategy} selected={selected} />
+      <Posts page={page} perPage={perPage} strategy={strategies[strategyIndex]} selected={selected} />
     </>
   )
 }
